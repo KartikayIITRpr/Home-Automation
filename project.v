df27393f6a737899c944068ad74736773506cf44
Kartikay Singh <kbsingh296@Kartikays-MacBook-Air.local>
2021-11-07 17:16:07 +0530

    revised modules and definition

--- project.v
+++ project.v
@@ -1,89 +1,109 @@
+// 16 : 1 multiplexer
 module mux_8(sel, inp, out) ; 
-  input [2:0] sel;
-  input [7:0] inp;
-  output out;
-  assign out = inp[sel];
+  input [2:0] sel ;
+  input [7:0] inp ;
+  output out ;
+  assign out = inp[sel] ;
 endmodule
 
+// 2 : 1 multiplexer
 module mux_2(sel, a, b, out) ;
-  input sel, a, b;
-  output out;
-  assign out = (sel) ? a:b;
+  input sel, a, b ;
+  output out ;
+  assign out = (sel) ? a:b ;
 endmodule
 
-module demux_8(sel, inp, out);
-  input [2:0] sel;
-  input [7:0][16:0] inp;
-  output [16:0] out;
+// 1 : 16 demultiplexer
+module demux_8(sel, inp, out) ;
+  input [2:0] sel ;
+  input [16:0] inp ;
+  output [7:0][16:0] out ;
 endmodule
 
+// This is the main module to control all the automations in the smart home.
+// All inputs will be passed from here.
 module smart_home(smokeDetector, doorState, windowState, humanDetector, 
                   doorEnable, in_password, change_password, rs_buttonState,
-                  e_buttonState, temperature, humanDetector, day_time, 
-                  motionSensor, stove_state);
-  input [3:0] smokeDetector, doorState, windowState, humanDetector, motionSensor ;
-  input [3:0] doorEnable, rs_buttonState, e_buttonState;
-  input [3:0][16:0] in_password, change_password;
-  input [4:0] day_time;
-  input [3:0][7:0] temperature ;
-  input garageState ,stove_state;
-  
+                  e_buttonState, temperature, humanDetector, luminosity, 
+                  motionSensor, stove_state) ;
+  input [2:0] smokeDetector, doorState, windowState, humanDetector, motionSensor ;
+  input [2:0] doorEnable, rs_buttonState, e_buttonState ;
+  input [2:0][16:0] in_password, change_password ;
+  input [2:0][2:0] luminosity ;
+  input [2:0][7:0] temperature ;
+  input garageState, stove_state ;
 endmodule
 
-module password_check(in_password, change_password, rs_buttonState, e_buttonState, lock, alarm) ;
-  reg [16:0] unique_key = 17'd45675;
-  reg [16:0] password;
-  input [16:0] in_password, change_password;
-  input rs_buttonState, e_buttonState;
-  integer count = 0;
-  output reg lock, alarm;
+// This module checks the password. It also has an option of reset, to reset the password if user forgets it.
+// We have registered a unique key, in case user forgets the password .
+module password_check(in_password, change_password, rs_buttonState, e_buttonState, unlock, alarm) ;
+  reg [16:0] unique_key = 17'd45675 ;
+  reg [16:0] password ;
+  input [16:0] in_password, change_password ;
+  input rs_buttonState, e_buttonState ;
+  integer count = 0 ;
+  output reg unlock, alarm ;
+  initial begin
+    unlock = 1'b0 ;
+    alarm = 1'b0 ;
+    password = unique_key ;
+  end
   always @(posedge e_buttonState) begin
     if (in_password == password || in_password == unique_key) begin
-      count = 0;
+      count = 0 ;
       if (rs_buttonState == 1'b0) begin
-        lock <= 1'b0;
+        unlock <= 1'b1 ;
+        unlock <= #1 1'b0 ;
       end
       else if (rs_buttonState == 1'b1) begin
-        password = change_password;
+        password = change_password ;
       end
     end
     else begin
-      count = count + 1;
+      count = count + 1 ;
     end
     if (count > 3) begin
-      alarm <= 1'b1;
+      alarm <= 1'b1 ;
+      alarm <= #1 1'b0 ;
     end
   end
 endmodule
 
-module airConditioning(temperature, heater, airConditioner, humanDetector, fan_speed);
+// This automated cooling and heating works with temperature and presence of humans as input.
+// It will turn A.C., heater on/off and control fan speed.  
+module airConditioning(temperature, heater, airConditioner, humanDetector, fan_speed) ;
   input [7:0] temperature ;
-  input humanDetector;
+  input humanDetector ;
   output heater, airConditioner ;
-  output [1:0] fan_speed;
-  
+  output [1:0] fan_speed ;  
 endmodule
 
-module lighting (day_time, motionSensor, light);
-  input [4:0] day_time;
-  input motion_sensor ;
+// It takes input, time and motion of humans, like in night time, when humans are not moving
+// It might be the case that he/she is sleeping or no one is in the room, hence lights are turned off.
+// In day time we do not need lights.
+module lighting (luminosity, motionSensor, light) ;
+  input [2:0] luminosity ;
+  input motionSensor ;
   output light ;
 endmodule
 
+// Chimney will turn on when stove is in an active state and turn off after some delay.
 module kitchen(stove_state, chimney) ;
   input stove_state ;
   output chimney ;
 endmodule
 
-/*alarm for each security system*/
-module fire_alarm(smoke_detector, alarmEnable);
-  input [3:0] smoke_detector ;
-  output alarmEnable;
+// Smoke luminosity is of multiple bits as there will be smoke detectors in every room.
+module fire_alarm(smoke_detector, alarmEnable) ;
+  input [2:0] smoke_detector ;
+  output alarmEnable ;
 endmodule
 
-module burglar_alarm(doorState, windowState, garageState, homeLocked, garageLocked, alarmEnable);
-  input [3:0] doorState, windowState;
-  input garageState;
-  input homeLocked, garageLocked;
-  output [3:0] alarmEnable;
+// This module will trigger burglar alarm in the respective room whenever the burglar breaks the door or window.
+// Burglar can enter from doors, windows or garage, so we have taken all of them in consideration.
+module burglar_alarm(doorState, windowState, garageState, homeLocked, garageLocked, alarmEnable) ;
+  input [2:0] doorState, windowState ;
+  input garageState ;
+  input homeLocked, garageLocked ;
+  output [2:0] alarmEnable ;
 endmodule
